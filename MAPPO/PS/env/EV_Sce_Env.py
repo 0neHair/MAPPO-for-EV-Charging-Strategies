c4567a30256825eaac004d157ff76f638c81ff9a
CQZ-ASUS <1019981667@qq.com>
2024-11-20 14:59:04 +0800

    modify1120

--- MAPPO/PS/env/EV_Sce_Env.py
+++ MAPPO/PS/env/EV_Sce_Env.py
@@ -6,7 +6,7 @@ Company: SEU
 from env.Multi_EV_Env import Multi_EV_Env
 from env.scenarios import load
 
-def EV_Sce_Env(args):
+def EV_Sce_Env(sce_name, seed=0, ps=False):
     '''
     Creates a MultiAgentEnv object as env. This can be used similar to a gym
     environment by calling env.reset() and env.step().
@@ -23,10 +23,10 @@ def EV_Sce_Env(args):
         .action_space       :   Returns the action space for each agent
         .n                  :   Returns the number of Agents
     '''
-    seed = args.seed if args.randomize else None
+    seed = seed
     # load scenario from script
-    scenario = load(args.sce_name + ".py").Scenario(frame=0.05, seed=seed)
+    scenario = load(sce_name + ".py").Scenario(frame=0.05, seed=seed)
     # create multiagent environment
-    env = Multi_EV_Env(scenario=scenario, seed=seed, args=args)
+    env = Multi_EV_Env(scenario=scenario, seed=seed, ps=ps)
 
     return env
