a7fdd3c606c5b69dc2f849ef99b521d38f2125c3
CQZ <1019981667@qq.com>
2024-11-30 23:21:33 +0800

    new alg init

---
+++ GA/GA.py
@@ -0,0 +1,31 @@
+'''
+Author: CQZ
+Date: 2024-11-30 21:29:03
+Company: SEU
+'''
+import numpy as np
+
+
+def schaffer(p):
+    '''
+    This function has plenty of local minimum, with strong shocks
+    global minimum at (0,0) with value 0
+    '''
+    x1, x2 = p
+    x = np.square(x1) + np.square(x2)
+    return 0.5 + (np.square(np.sin(x)) - 0.5) / np.square(1 + 0.001 * x)
+
+from sko.GA import GA
+
+ga = GA(func=schaffer, n_dim=2, size_pop=50, max_iter=800, prob_mut=0.001, lb=[-1, -1], ub=[1, 1], precision=1e-7)
+best_x, best_y = ga.run()
+print('best_x:', best_x, '\n', 'best_y:', best_y)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Y_history = pd.DataFrame(ga.all_history_Y)
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(Y_history.index, Y_history.values, '.', color='red')
+Y_history.min(axis=1).cummin().plot(kind='line')
+plt.show()
